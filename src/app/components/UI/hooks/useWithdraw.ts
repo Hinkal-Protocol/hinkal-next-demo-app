04a427edbd28d9687409c2ba46b66e2961e41a29
sabaaa1 <sdeli23@freeuni.edu.ge>
2026-02-27 08:58:03 +0400

    minor changes

--- src/app/components/UI/hooks/useWithdraw.ts
+++ src/app/components/UI/hooks/useWithdraw.ts
@@ -20,7 +20,7 @@ export const useWithdraw = ({
       token: ERC20Token,
       amount: string,
       recipientAddress: string,
-      isRelayerOff: boolean
+      isRelayerOff: boolean,
     ) => {
       if (!hinkal) {
         throw new Error("Hinkal instance not initialized");
@@ -39,7 +39,7 @@ export const useWithdraw = ({
           undefined,
           undefined,
           undefined,
-          false
+          false,
         );
 
         if (typeof tx === "bigint") {
@@ -57,7 +57,7 @@ export const useWithdraw = ({
         setIsProcessing(false);
       }
     },
-    [hinkal, onSuccess, onError]
+    [hinkal, onSuccess, onError],
   );
 
   return {

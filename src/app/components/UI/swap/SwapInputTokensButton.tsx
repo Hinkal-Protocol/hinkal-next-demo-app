ce645b48825458390942d075537c8aae0d75e64e
sabaaa1 <sdeli23@freeuni.edu.ge>
2026-02-27 09:16:07 +0400

    added svg

--- src/app/components/UI/swap/SwapInputTokensButton.tsx
+++ src/app/components/UI/swap/SwapInputTokensButton.tsx
@@ -2,12 +2,27 @@ interface SwapInputTokensButtonProps {
   onClick?: () => void;
 }
 
-export const SwapInputTokensButton = ({ onClick }: SwapInputTokensButtonProps) => (
+export const SwapInputTokensButton = ({
+  onClick,
+}: SwapInputTokensButtonProps) => (
   <button
     type="button"
     onClick={onClick}
-    className="h-full mx-auto border-2 py-2 px-5 m-2 rounded-xl cursor-pointer text-[16px] font-bold"
+    className="h-full mx-auto border-2 border-white/20 py-2 px-5 m-2 rounded-xl cursor-pointer bg-white/10 hover:bg-white/20 transition-colors"
   >
-    <i className="bi bi-arrow-down-up" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M7 16V4m0 0L3 8m4-4l4 4" />
+      <path d="M17 8v12m0 0l4-4m-4 4l-4-4" />
+    </svg>
   </button>
 );

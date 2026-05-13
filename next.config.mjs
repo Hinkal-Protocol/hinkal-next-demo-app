04d51bf4c74db53de9a0f3dda6212d050ef72eea
AkakiGurgenidze <agurgenidze02@gmail.com>
2026-05-13 18:13:54 +0400

    remove webpack changes

--- next.config.mjs
+++ next.config.mjs
@@ -12,17 +12,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        worker_threads: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
